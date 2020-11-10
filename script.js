9cec4b62d0ae09240ba183cd69b7a84543d947be
Raymond Wolt <rjwolt@gmail.com>
2020-11-09 19:57:00 -0600

    Two operator operations working

--- script.js
+++ script.js
@@ -24,22 +24,25 @@ function negative (a) {
 
 function operate (num1, num2, operator) {
     switch(operator) {
-        case add:
+        case 'add':
             return add(num1, num2);
             break;
-        case subtract:
+        case 'subtract':
             return subtract(num1, num2);
             break;
-        case multiply:
+        case 'multiply':
             return multiply(num1, num2);
             break;
-        case divide:
+        case 'divide':
             return multiply(num1, num2);
     }
 }
 
 let screen = document.querySelector('.calc-screen');
 let displayValue = screen.textContent;
+let operator;
+let num1;
+let num2;
 
 let numbers = document.querySelectorAll('.number');
 for (let button of numbers) {
@@ -54,8 +57,32 @@ for (let button of numbers) {
     });
 }
 
+let operators = document.querySelectorAll('.operator');
+for (let button of operators) {
+    button.addEventListener('click', () => {
+        num1 = +displayValue;
+        displayValue = '';
+        operator = button.id;
+    });
+}
+
+let equalBtn = document.querySelector('#equals');
+equalBtn.addEventListener('click', () => {
+    num2 = +displayValue;
+    let solution = operate(num1, num2, operator);
+    screen.textContent = solution;
+});
+
+
 let clearBtn = document.querySelector('#clear');
 clearBtn.addEventListener('click', () => {
     displayValue = '';
     screen.textContent = displayValue;
-})
+});
+
+let percentBtn = document.querySelector('#percent');
+percentBtn.addEventListener('click', () => {
+    displayValue = +displayValue / 100;
+    screen.textContent = displayValue;
+});
+
