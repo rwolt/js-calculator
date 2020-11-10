48ad0926fa17aa3ae58fb9f7fb47ee12e1044ac6
Raymond Wolt <rjwolt@gmail.com>
2020-11-09 20:48:21 -0600

    Allows stringing together operations

--- script.js
+++ script.js
@@ -14,14 +14,6 @@ function divide (a, b) {
     return a / b;
 }
 
-function percent (a) {
-    return a / 100;
-}
-
-function negative (a) {
-    return a * -1;
-}
-
 function operate (num1, num2, operator) {
     switch(operator) {
         case 'add':
@@ -34,7 +26,11 @@ function operate (num1, num2, operator) {
             return multiply(num1, num2);
             break;
         case 'divide':
+            if (num2 == 0) {
+                return 'LoL'
+            } else {
             return multiply(num1, num2);
+            }
     }
 }
 
@@ -60,9 +56,17 @@ for (let button of numbers) {
 let operators = document.querySelectorAll('.operator');
 for (let button of operators) {
     button.addEventListener('click', () => {
+        if (num1) {
+            num2 = +displayValue;
+            displayValue = '';
+            num1 = operate(num1, num2, operator);
+            operator = button.id;
+            screen.textContent = num1;
+        } else {
         num1 = +displayValue;
         displayValue = '';
         operator = button.id;
+        }
     });
 }
 
@@ -73,16 +77,27 @@ equalBtn.addEventListener('click', () => {
     screen.textContent = solution;
 });
 
-
 let clearBtn = document.querySelector('#clear');
 clearBtn.addEventListener('click', () => {
     displayValue = '';
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
     screen.textContent = displayValue;
 });
 
 let percentBtn = document.querySelector('#percent');
 percentBtn.addEventListener('click', () => {
+    displayValue = screen.textContent;
     displayValue = +displayValue / 100;
     screen.textContent = displayValue;
 });
+
+let negateBtn = document.querySelector('#negate');
+negateBtn.addEventListener('click', () => {
+    displayValue = +displayValue * -1;
+    screen.textContent = displayValue;
+});
+
+
 
